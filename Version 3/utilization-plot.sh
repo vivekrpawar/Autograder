459b3269ea4f40a0ae9200884ac917322385c7c5
vivekrpawar <vivekpawar189@gmail.com>
2023-10-22 11:42:18 +0530

    bashscript directory created unnecessary files removed

--- Version 3/utilization-plot.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-cat utilization_results.txt | graph -T png --bitmap-size "1400x1400" -g 3 -L "Clients vs CPU utilization" -X "Number of Clients" -Y "Average CPU utilization (in %)" -r 0.25> ./utilization_plot.png
